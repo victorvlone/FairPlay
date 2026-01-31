a21945811389534ce5937c8ca4d189e4f958979e
João Victor <victorvlone999@gmail.com>
2026-01-31 13:02:45 -0300

    first commit

---
+++ fair/src/main/java/com/fairplay/fair/FairApplication.java
@@ -0,0 +1,13 @@
+package com.fairplay.fair;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FairApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FairApplication.class, args);
+	}
+
+}
---
+++ fair/src/test/java/com/fairplay/fair/FairApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fairplay.fair;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FairApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
